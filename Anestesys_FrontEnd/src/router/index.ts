930ebd3643531e7c9cc41644924d4f8bcc795494
EstebanSerrano27 <104015786+EstebanSerrano27@users.noreply.github.com>
2023-02-27 15:11:30 -0600

    Update index.ts

--- Anestesys_FrontEnd/src/router/index.ts
+++ Anestesys_FrontEnd/src/router/index.ts
@@ -1,5 +1,6 @@
 import { useUserStore } from '@/stores/user-store';
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter,
+         createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
