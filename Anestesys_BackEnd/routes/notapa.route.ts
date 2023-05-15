912814554345a56dddc7c89f20a5a01a582d016e
Luis Esteban Serrano <luis.serrano@biossmann.com>
2023-05-15 17:04:41 -0600

    ANW-55 creada función de listado de nota PostAnestésica

--- Anestesys_BackEnd/routes/notapa.route.ts
+++ Anestesys_BackEnd/routes/notapa.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { requireToken } from "../middlewares/requireToken";
 import { saveNotaPA,
-         updateNotaPA } from "../controllers/postanest.controller";
+         updateNotaPA,
+         listNotaPA } from "../controllers/postanest.controller";
 
 const router = Router();
 
 /*------------------- PreId --------------------*/
 router.post('/', requireToken, saveNotaPA);
 router.put('/:id',  requireToken, updateNotaPA);
+router.get('/:id', requireToken, listNotaPA);
 
 export default router;
