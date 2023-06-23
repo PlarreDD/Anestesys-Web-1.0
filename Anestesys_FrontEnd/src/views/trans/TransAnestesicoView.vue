df5df805d54e9e1bf8f24b31d4d31e682664b10a
Daniel Hernández~ <juan.hernandez@biossmann.com>
2023-06-23 13:54:16 -0600

    Bloqeuar boton de inicio monitoreo

--- Anestesys_FrontEnd/src/views/trans/TransAnestesicoView.vue
+++ Anestesys_FrontEnd/src/views/trans/TransAnestesicoView.vue
@@ -18,8 +18,9 @@
               </template>
 
               <template v-if="transAnestStore.envDat === false && medStore.status === 'Activo'">
-                <img src="images/imgIcon/MonitorActivo.png" @click="iniMSV" :disabled="btnMSV" style="width: 70px; height: auto; cursor: pointer;"/>
-                <span class="fw-bold" style="color:#002D60">&nbsp;&nbsp;Estado: Sin Datos</span>
+                <!-- <img src="images/imgIcon/MonitorActivo.png" @click="iniMSV" :disabled="btnMSV" style="width: 70px; height: auto; cursor: pointer;"/> -->
+                <input type="image" src="images/imgIcon/MonitorActivo.png" @click="iniMSV" :disabled="btnMSV" style="width: 70px; height: auto; cursor: pointer;">  
+                <span class="fw-bold" style="color:#002D60; vertical-align: super;">&nbsp;&nbsp;Estado: Sin Datos</span>
               </template>  
 
               <template v-if="medStore.status === 'Inactivo'">
