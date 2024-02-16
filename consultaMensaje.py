abfd125f66a66b0b917e7f47dc55f9f465d28933
EstebanSerrano27 <104015786+EstebanSerrano27@users.noreply.github.com>
2024-02-16 09:37:53 -0600

    Actualizado para permitir conexiones de todos

--- consultaMensaje.py
+++ consultaMensaje.py
@@ -3,11 +3,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-informacion_variable = "Hola, esta es la información que quiero almacenar."
+informacion_variable = "Que chingue a su madre el Dani"
 
 @app.route('/obtener_informacion', methods=['GET'])
 def obtener_informacion():
     return jsonify({'informacion': informacion_variable})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
