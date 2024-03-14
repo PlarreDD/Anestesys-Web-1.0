2175d915983f128c663d1de68d7c296d917dc47f
Luis Esteban Serrano <luis.serrano@biossmann.com>
2024-03-14 16:16:32 -0600

    Actualizacion de la apiMSV con el icono

--- apiMSV/Properties/Resources.Designer.cs
+++ apiMSV/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace apiMSV.Properties {
         }
         
         /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Icon similar a (Icono).
+        /// </summary>
+        internal static System.Drawing.Icon apiMVS {
+            get {
+                object obj = ResourceManager.GetObject("apiMVS", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Fondo {
