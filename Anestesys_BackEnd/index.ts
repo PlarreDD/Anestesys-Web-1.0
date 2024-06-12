939a7feee64a8721af777dc3835bf2360af47de5
Daniel Hernández <juan.hernandez@biossmann.com>
2024-06-12 13:12:44 -0600

    Se quita la sección de teclado virtual

--- Anestesys_BackEnd/index.ts
+++ Anestesys_BackEnd/index.ts
@@ -14,7 +14,6 @@ import recuperaRouter from './routes/recuperacion.route';
 import notapaRouter from './routes/notapa.route';
 import transRouter from "./routes/datosv.route";
 import mvsRouter from "./routes/mvs.route";
-import { exec } from 'child_process';
 
 const net = require('net');
 const app = express();
@@ -63,19 +62,6 @@ app.get('/api/getClienteIp', (req, res) => {
     res.json({ clienteIp });
     // console.log("IP que manda solicitud:" + clienteIp);    
   }
-});
-
-// Nueva ruta para abrir el teclado en pantalla
-app.post('/api/openKeyboard', (_req, res) => {
-  exec('osk', (error: Error | null, stdout: string, stderr: string) => {
-    if (error) {
-      console.error(`Error al abrir el teclado en pantalla: ${error}`);
-      return res.status(500).send('Error al abrir el teclado en pantalla');
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-    res.send('Teclado en pantalla abierto');
-  });
 });
 
 const PORT = process.env.PORT || 5000;
