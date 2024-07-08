f6061a806baaa4523a2a1f6202497b305bb97603
Luis Esteban Serrano <leserr267@gmail.com>
2024-07-08 16:00:25 -0600

    Creada función de guardado de estudios

--- Anestesys_BackEnd/controllers/cirugias.controller.ts
+++ Anestesys_BackEnd/controllers/cirugias.controller.ts
@@ -65,22 +65,14 @@ export const saveMSVdat = async (req: any, res: Response) => {
                 PAM_IN: any; FiCO2: any;
                 FR: any; HoraGeneracion: any;
             }) => ({
-                FC: data.FC,
-                Pulso: data.Pulso,
-                PAS: data.PAS,
-                PAD: data.PAD,
-                PAM: data.PAM,
-                SpO2: data.SpO2,
-                EtCO2: data.EtCO2,
-                Temp1: data.Temp1,
-                Temp2: data.Temp2,
-                PVC: data.PVC,
-                PAS_IN: data.PAS_IN,
-                PAD_IN: data.PAD_IN,
-                PAM_IN: data.PAM_IN,
-                FiCO2: data.FiCO2,
-                FR: data.FR,
-                HoraGeneracion: data.HoraGeneracion,
+                FC: data.FC, Pulso: data.Pulso,
+                PAS: data.PAS, PAD: data.PAD,
+                PAM: data.PAM, SpO2: data.SpO2,
+                EtCO2: data.EtCO2, Temp1: data.Temp1,
+                Temp2: data.Temp2, PVC: data.PVC,
+                PAS_IN: data.PAS_IN, PAD_IN: data.PAD_IN,
+                PAM_IN: data.PAM_IN, FiCO2: data.FiCO2,
+                FR: data.FR, HoraGeneracion: data.HoraGeneracion,
                 }));
 
         const cirugia = await Cirugias.findOneAndUpdate(
@@ -95,4 +87,29 @@ export const saveMSVdat = async (req: any, res: Response) => {
         });
         return res.status(500).json({Error: 'Error de servidor'});
     }
-}
+};
+
+export const saveEstudios = async (req: any, res: Response) => {
+    try {
+        const { pid,
+                val_Estudios } = req.body;
+        
+        const valest = await Cirugias.findOneAndUpdate(
+            { pid: pid },
+            {$push: {
+                val_Estudios: {
+                    estudio: val_Estudios[0],
+                    especifEstudio: val_Estudios[1]
+                }
+            }}
+        )
+
+        return res.json({ valest });
+    } catch (error) {
+        logger.log({
+            level: 'error',
+            message: 'Error de servidor', error
+        });
+        return res.status(500).json({Error: 'Error de servidor'});
+    }
+};
